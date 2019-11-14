a745cb59c21e187810e7f437edb2a0fd85a3cb6c
Nermin Kekic <nermink@gmail.com>
2019-11-14 11:39:44 -0600

    added check for user guess

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is Hangman game writen in Python.
+The objective of the game is:
+    1. Script will pick a random word from a list of words.
+    2. It will then promp you to guess what letter is in the word. 
+    3. You have total of 6 guesses to correctly guess the word. 
+    4. If you guess correctly the word with 6 guesses or less, you win.
+    5. If you are unable to guess in 6 guesses you lose the game. 
--- hangman.py
+++ hangman.py
@@ -11,9 +11,35 @@ random.shuffle(word_list)
 # Variable to track guesses
 guess_count = 0
 # User guess
-user_gueess = ""
-# Extract word for guessing
+player_guess = ""
+# Extract word for list
+# Then index each letter in word
+# Make all letters lowercase
 word = list(word_list[0])
+word = "".join(word)
+word = word.lower()
+# Replace each letter in word with dash
+display_dash = []
+for i in range(len(word)):
+    display_dash.append("_")
 
 
-print(word)
+# Display dash instead of the word
+print(*display_dash, sep=" ")
+
+print("You have 6 chances to guess correctly the word.")
+print("The word is hiden by dashes.")
+print("To guess the word, you must provide letter that is part of the word.")
+
+
+# Get the letter from player
+player_guess = input("To proceed, enter letter that is part of the word: ")
+
+# Convert display_dash to list so that we can manipulate elements
+display_dash = list(display_dash)
+
+for i, letter_in_word in enumerate(word):
+    if player_guess == letter_in_word:
+        display_dash[i] = player_guess
+        print(display_dash)
+
